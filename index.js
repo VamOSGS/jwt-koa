882d8afce94b770f66c09461e1988d00c6afe150
VamOSGS <vamosgs2001@gmail.com>
2018-01-24 20:58:23 +0400

    Middleware added

---
+++ index.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken');
+
+const { secret } = process.env;
+module.exports = (ctx, next) => {
+    const token = ctx.request.body.token || ctx.headers.authorization;
+    if (token) {
+        jwt.verify(token, secret, (err) => {
+            if (err) {
+                ctx.body = {
+                    message: 'BAD TOKEN',
+                };
+            } else {
+                next();
+            }
+        });
+    } else {
+        ctx.body = {
+            message: 'NO TOKEN',
+        };
+    }
+};
